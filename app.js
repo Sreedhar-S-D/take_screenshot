644f39c140f10c118a79f00833a9430d98573a27
Sreedhar Dhulkhed <ssdhulkhed@gmail.com>
2020-11-10 19:09:21 +0530

    enjoy pics

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let canvas = document.querySelector('#canvas');
+let context = canvas.getContext('2d');
+let video = document.querySelector('#video');
+
+
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video: true}).then(stream =>{
+        video.srcObject = stream;
+        video.play();
+    })
+}
+
+document.getElementById('snap').addEventListener('click', ()=>{
+    context.drawImage(video, 0, 0, 640 , 480);
+    domtoimage.toBlob(document.getElementById('canvas')).then(function(blob){
+        window.saveAs(blob, 'output.jpg');
+    });
+});
